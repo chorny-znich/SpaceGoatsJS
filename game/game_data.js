649472ae6df9f14765d4fa15730759a0c3bb15de
chorny-znich <vargar.dt@gmail.com>
2024-03-09 21:21:58 +0300

    feat: add the class with the game metadata

---
+++ game/game_data.js
@@ -0,0 +1,10 @@
+export default class GameData {
+  static GAME_WIDTH = 1200;
+  static GAME_HEIGHT = 800;
+  static CELL_SIZE = 100;
+  static NUMBER_OF_CELLS = 72;
+  static LEFT_GRID_START = 0; // left position of cell's grid on canvas
+  static TOP_GRID_START = 100; // top position of cell's grid on canvas
+  static GRID_WIDTH = 12;
+  static GRID_HEIGHT = 6;
+}
