19cffc8d8ae8261b72bfe3dbd2b23604e67286f3
chorny-znich <vargar.dt@gmail.com>
2024-03-09 22:15:46 +0300

    feat: create the instance of the game class in main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//import Game from "./game/game.js"
+import Game from "./game/game.js"
 import GameData from "./game/game_data.js";
 
 window.addEventListener('load' , () => {
@@ -9,15 +9,15 @@ window.addEventListener('load' , () => {
 
   let totalPlayTime = 0;
 
-//  const game = new Game(canvas);
-//  game.init();
+  const game = new Game(canvas);
+  game.init();
   // game cycle
   function gameCycle(timestamp) {
     const dt = (timestamp - totalPlayTime) / 1000;
     totalPlayTime = timestamp;
     context.clearRect(0, 0, canvas.width, canvas.height);
-  //  game.update(dt);
-  //  game.render();
+    game.update(dt);
+    game.render();
     requestAnimationFrame(gameCycle);
   }
   gameCycle(0);
